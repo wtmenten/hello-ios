87b2d474ac08c71840bffcb46b4d8a9425f15224
wtmenten <wtmenten@gmail.com>
2016-09-30 16:54:53 -0700

    first homework

--- Hello-iOS/ViewController.swift
+++ Hello-iOS/ViewController.swift
@@ -5,21 +5,35 @@
 //  Created by iGuest on 9/30/16.
 //  Copyright © 2016 iGuest. All rights reserved.
 //
-
+import Foundation
 import UIKit
 
-class ViewController: UIViewController {
+extension UIView {
+    func fadeIn(duration: TimeInterval = 1.0, delay: TimeInterval = 0.0, completion: @escaping ((Bool) -> Void) = {(finished: Bool) -> Void in}) {
+        UIView.animate(withDuration: duration, delay: delay, options: UIViewAnimationOptions.curveEaseIn, animations: {
+            self.alpha = 1.0
+            }, completion: completion)  }
+    
+    func fadeOut(duration: TimeInterval = 1.0, delay: TimeInterval = 0.0, completion: @escaping (Bool) -> Void = {(finished: Bool) -> Void in}) {
+        UIView.animate(withDuration: duration, delay: delay, options: UIViewAnimationOptions.curveEaseIn, animations: {
+            self.alpha = 0.0
+            }, completion: completion)
+    }
+}
 
+class ViewController: UIViewController {
+    @IBOutlet weak var hello: UILabel?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.hello?.alpha = 0.0
+        self.hello?.fadeIn(duration:2.0, delay:1.0)
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
 }
-
